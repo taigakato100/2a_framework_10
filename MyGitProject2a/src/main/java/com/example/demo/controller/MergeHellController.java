e731e42b02dc9ddc316c5f4e756e7afbddbf54ca
S0610 <S0610@22S0610>
2023-11-14 15:11:56 +0900

    aa

---
+++ MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+		kadaiB();
+		kadaiC();
+
+	void kadaiC() {
+		int a;
+		a = 50;
+
+		int b;
+		b = 75;
+
+		int c;
+		c = a + b;
+
+	}
+}
