d4de10f5ab2241a2e0db38e449aed609a9da4346
S0625 <S0625@22S0625>
2023-11-20 10:00:52 +0900

    naosita

--- MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
+++ MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -3,15 +3,33 @@ package com.example.demo.controller;
 import java.util.Random;
 
 public class MergeHellController {
-	public static void main(String[] args) {
+	public void main(String[] args) {
 		kadaiA();
 		kadaiB();
 		kadaiC();
+	}
+
+	void kadaiA() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "回目のループだよ");
+		}
 	}
 
 	void kadaiB() {
 		Random rand = new Random();
 		int kasu = (rand.nextInt() + 1) % 100;
 		System.out.println(kasu + "が出ました");
+	}
+
+	void kadaiC() {
+		int a;
+		a = 50;
+
+		int b;
+		b = 75;
+
+		int c;
+		c = a + b;
+		System.out.println(c);
 	}
 }
